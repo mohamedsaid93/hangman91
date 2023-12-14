0bec8036af1c76fa03a255f0eebb5eca5749ca2e
Mohamed Said <msm.793@hotmail.com>
2023-12-14 20:50:48 +0000

    milestone 5 completed

---
+++ milestone_5.py
@@ -0,0 +1,74 @@
+import random
+word_list = ["orange", "apple","banana","watermelon","mango"]
+
+
+class Hangman:
+    def __init__(self, word_list, num_lives=5):
+        self.word_list = word_list
+        self.num_lives = num_lives
+        self.list_of_guesses = []
+        self.word = random.choice(word_list).lower()
+        self.word_guessed = ['_'] * len(self.word)
+        self.num_letters = len(set(self.word))
+
+    
+    def check_guess(self, guess):
+        guess = guess.lower()
+
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+
+            for i, letter in enumerate(self.word):
+                if letter == guess:
+                    self.word_guessed[i] = guess
+
+            self.num_letters -= 1
+            print(f"You have {self.num_letters} litters left.")
+
+        else:
+            self.num_lives -= 1
+            print(f"Sorry, {guess} is not in the word.")
+            print(f"You have {self.num_lives} lives left.")
+            
+
+ 
+    def ask_for_input(self):
+        while True:
+            guess = input("Guess a letter: ") 
+
+            
+            if not guess.isalpha() or len(guess) != 1:
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+                break
+            
+
+
+def play_game(word_list):
+
+    num_lives = 5
+    game = Hangman(word_list, num_lives)
+
+
+    while True:
+        if game.num_lives == 0:
+            print("You lost")
+            break
+
+        elif game.num_letters > 0:
+            game.ask_for_input()
+
+        else:
+            print("Congratulations. You won the game!")
+            break
+        
+
+play_game(word_list)
+
+
+
+
